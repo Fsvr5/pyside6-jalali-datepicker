64fa31e211c6fa200901d234e9501c03d3289789
Fsvr5 <74720058+Fsvr5@users.noreply.github.com>
2026-08-29 12:26:31 +0330

    feat: export calendar marker API

--- src/jalali_datepicker/__init__.py
+++ src/jalali_datepicker/__init__.py
@@ -1,14 +1,17 @@
 from .calendar import JalaliCalendarWidget
+from .markers import DayMarker, MarkerStore
 from .popup import JalaliPopupDatePicker
 from .themes import Theme
 from .widgets import JalaliDateEdit, JalaliDatePicker, JalaliDateRangeEdit
 
 __all__ = [
+    "DayMarker",
     "JalaliCalendarWidget",
     "JalaliDateEdit",
     "JalaliDatePicker",
     "JalaliDateRangeEdit",
     "JalaliPopupDatePicker",
+    "MarkerStore",
     "Theme",
 ]
-__version__ = "0.3.0"
+__version__ = "0.4.0"
