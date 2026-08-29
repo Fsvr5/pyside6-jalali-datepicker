b88233761a681b8203b176d73152168781f5a645
Fsvr5 <74720058+Fsvr5@users.noreply.github.com>
2026-08-29 12:23:14 +0330

    feat: export custom Jalali popup calendar

--- src/jalali_datepicker/__init__.py
+++ src/jalali_datepicker/__init__.py
@@ -1,5 +1,14 @@
+from .calendar import JalaliCalendarWidget
+from .popup import JalaliPopupDatePicker
 from .themes import Theme
 from .widgets import JalaliDateEdit, JalaliDatePicker, JalaliDateRangeEdit
 
-__all__ = ["JalaliDateEdit", "JalaliDatePicker", "JalaliDateRangeEdit", "Theme"]
-__version__ = "0.2.0"
+__all__ = [
+    "JalaliCalendarWidget",
+    "JalaliDateEdit",
+    "JalaliDatePicker",
+    "JalaliDateRangeEdit",
+    "JalaliPopupDatePicker",
+    "Theme",
+]
+__version__ = "0.3.0"
